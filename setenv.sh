031a8bd42a232035ad8e18b140915166be3abbe3
Benediktas Rukas <39025147+benycode@users.noreply.github.com>
2022-01-22 17:39:28 +0200

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-JAVA_OPTS="$JAVA_OPTS -Dtsp-source=file:/dss/tsp-config.xml -Dhttps.proxyHost=proxy-balancer -Dhttps.proxyPort=5000 -Dhttp.proxyHost=proxy-balancer -Dhttp.proxyPort=5000"
+JAVA_OPTS="$JAVA_OPTS -Dtsp-source=file:/dss/tsp-config.xml -Dproxy.http.enabled=true -Dproxy.http.host=proxy-balancer -Dproxy.http.port=5000 -Dproxy.https.enabled=true -Dproxy.https.host=proxy-balancer -Dproxy.https.port=5000"
